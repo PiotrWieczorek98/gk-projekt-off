28c6a043956177ec2156a1042fe3169df88ed26a
paultro708 <32535575+paultro708@users.noreply.github.com>
2020-05-13 18:44:31 +0200

    fix door animation

--- Assets/Scripts/Opener.cs
+++ Assets/Scripts/Opener.cs
@@ -5,25 +5,53 @@ using UnityEngine;
 
 public class Opener : MonoBehaviour
 {
-    private float time=0.25f;
-    //current state
+    //current state of object
     private bool isOpen = false;
-    //opening object
-    public GameObject obj = null;
     public Animator animator;
+    private bool inTrigger = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = (Animator)obj.GetComponent<Animator>();
+        //obj = (GameObject)gameObject.GetComponent<GameObject>;
+        animator = (Animator)this.GetComponent<Animator>();
         Debug.Log("started");
+        //time = 1f;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        time += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.E)&&time > 0)
+        //time += Time.deltaTime;
+        //if (Input.GetKeyDown(KeyCode.E)&&time > 0)
+        //{
+        //    Debug.Log("E");
+        //    if (!isOpen)
+        //    {
+        //        animator.SetTrigger("OpenDoor");
+        //        isOpen = true;
+        //    }
+        //    else
+        //    {
+        //        animator.SetTrigger("CloseDoor");
+        //        isOpen = false;
+        //    }
+        //    time -= Time.deltaTime;
+        //}
+        //if (time < 0)
+        //    time = 1f;
+        inTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inTrigger)
         {
             Debug.Log("E");
             if (!isOpen)
@@ -36,14 +64,7 @@ public class Opener : MonoBehaviour
                 animator.SetTrigger("CloseDoor");
                 isOpen = false;
             }
-            time -= Time.deltaTime;
+            //time -= Time.deltaTime;
         }
-        if (time < 0)
-            time = 1f;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 }

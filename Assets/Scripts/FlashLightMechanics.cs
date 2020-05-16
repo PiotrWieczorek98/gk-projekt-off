5425d35bdc95a1cd780cfa083a12b6de80deea3b
Dominika Matyja <dominika.matyja@siroccomobile.com>
2020-05-16 17:52:52 +0200

    Added flashlight for player.

---
+++ Assets/Scripts/FlashLightMechanics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightMechanics : MonoBehaviour
+{
+    public bool isFlashlightOn = false;
+    public bool flashlightDelay = false;
+
+    public GameObject flashlight;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            flashlightDelay = true;
+            isFlashlightOn = !isFlashlightOn;
+            flashlight.SetActive(isFlashlightOn);
+            StartCoroutine(FlashlightDelay());
+        }
+    }
+
+    IEnumerator FlashlightDelay()
+    {
+        yield return new WaitForSeconds(0.25f);
+        flashlightDelay = false;
+    }
+}

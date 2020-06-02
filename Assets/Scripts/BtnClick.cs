16f13e39cf0498dba82ccf3ab214d52847ad874d
Dominika Matyja <dominika.matyja@siroccomobile.com>
2020-06-02 13:14:18 +0200

    Changes in final scene.

---
+++ Assets/Scripts/BtnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnClick : MonoBehaviour
+{
+    private bool isClicked;
+    private Animator animator;
+
+
+    void Start()
+    {
+        animator = (Animator)this.GetComponent<Animator>();
+        isClicked = animator.GetBool("state");
+
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && other.CompareTag("Player"))
+        {
+            if (!isClicked)
+            {
+                animator.SetBool("state", true);
+                isClicked = true;
+            }
+            //time -= Time.deltaTime;
+        }
+    }
+}

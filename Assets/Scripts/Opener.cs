8401e6afaedfaae13cb8f67585dbfb7f567e0af5
paultro708 <32535575+paultro708@users.noreply.github.com>
2020-06-16 22:20:07 +0200

    fix village scene

--- Assets/Scripts/Opener.cs
+++ Assets/Scripts/Opener.cs
@@ -5,30 +5,63 @@ using UnityEngine;
 
 public class Opener : MonoBehaviour
 {
-    private bool isOpen;
-    private Animator animator;
+    //current state of object
+    private bool isOpen = false;
+    public Animator animator;
+    private bool inTrigger = false;
 
 
+    // Start is called before the first frame update
     void Start()
     {
+        //obj = (GameObject)gameObject.GetComponent<GameObject>;
         animator = (Animator)this.GetComponent<Animator>();
-        isOpen = animator.GetBool("state");
-
+        Debug.Log("started");
+        //time = 1f;
     }
 
-
-    void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.E) && other.CompareTag("Player") )
+        //time += Time.deltaTime;
+        //if (Input.GetKeyDown(KeyCode.E)&&time > 0)
+        //{
+        //    Debug.Log("E");
+        //    if (!isOpen)
+        //    {
+        //        animator.SetTrigger("OpenDoor");
+        //        isOpen = true;
+        //    }
+        //    else
+        //    {
+        //        animator.SetTrigger("CloseDoor");
+        //        isOpen = false;
+        //    }
+        //    time -= Time.deltaTime;
+        //}
+        //if (time < 0)
+        //    time = 1f;
+        inTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inTrigger)
         {
+            Debug.Log("E");
             if (!isOpen)
             {
-                animator.SetBool("state", true);
+                animator.SetTrigger("OpenDoor");
                 isOpen = true;
             }
             else
             {
-                animator.SetBool("state", false);
+                animator.SetTrigger("CloseDoor");
                 isOpen = false;
             }
             //time -= Time.deltaTime;

7040514e3f0243126c4f843a146b97cd46fcce96
Dominika Matyja <dominika.matyja@siroccomobile.com>
2020-06-18 15:15:04 +0200

    Scene change.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Start()
+    {
+        completeLevelUI.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}

d1a51ed796c127d8588e59ae472e4459e6254f3b
Dominika Matyja <dominika.matyja@siroccomobile.com>
2020-06-18 16:43:31 +0200

    Kreditsy.

--- Assets/Scripts/BtnClick.cs
+++ Assets/Scripts/BtnClick.cs
@@ -8,17 +8,14 @@ public class BtnClick : MonoBehaviour
     private bool isClicked = false;
     public int iterator = 0;
     public Image endScreen;
-    // public Button endButton;
     private Animator animator;
 
+    public GameManager gameManager;
 
     void Start()
     {
-        ////animator = (Animator)this.GetComponent<Animator>();
         isClicked = animator.GetBool("state");
         endScreen.enabled = false;
-        //endButton.enabled = false;
-        //uiObject.SetActive(false);
     }
 
     void OnTriggerStay(Collider other)
@@ -27,27 +24,11 @@ public class BtnClick : MonoBehaviour
         {
             if (!isClicked)
             {
-                //animator.SetBool("state", true);
                 isClicked = true;
             }
-            //time -= Time.deltaTime;
         }
 
         if (isClicked)
-            iterator++;
-
-        if (iterator == 300)
-        {
-             endScreen.enabled = true;
-            //endButton.enabled = true;
-
-        }
-            
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Quitting game...");
-        Application.Quit();
+            gameManager.CompleteLevel();
     }
 }
